e9846092d304b54a79d980783a04498329620ac1
kannav4199 <kannav4199@gmail.com>
2019-12-12 00:16:25 +0530

    ok
    ok

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $message = $_POST['message'];
 
 
-$email_from = 'kannav4199@.com';
+$email_from = 'kannav4199@gmail.com';
 
 $email_subject = "New Form Submission";
 
